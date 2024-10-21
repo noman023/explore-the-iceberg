1f34f728af45d0427c1407ad23b48b3c95d9dd50
Mojammel Noman <molla.noman23@gmail.com>
2024-10-21 23:48:54 +0600

    connect database with mongoose

--- express-101/app.js
+++ express-101/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+const port = process.env.PORT || 400;
 
 app.use(require("./routes/index"));
 
@@ -8,6 +11,27 @@ app.use((req, res) => {
   res.send("Page not found!");
 });
 
-app.listen(400, () => {
-  console.log("server is running on 400 port");
-});
+// Replace with your actual MongoDB password
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rmap.mongodb.net/Test?retryWrites=true&w=majority`;
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Connecting to MongoDB
+    await mongoose.connect(uri, clientOptions);
+    // This line confirms the connection
+    console.log("Connected to MongoDB!");
+
+    // Start the Express server after successful DB connection
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+run().catch(console.dir);
